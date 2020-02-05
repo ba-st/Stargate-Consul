524614fcb99676d3f362d628d8e37ae6ad345eda
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2020-02-05 10:17:43 -0300

    Improve script

--- compose-test.sh
+++ compose-test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 echo "TRAVIS_BRANCH=${TRAVIS_BRANCH}" > .env
-docker-compose -f api-tests/docker-compose.yml up -d
+docker-compose -f api-tests/docker-compose.yml up -d || exit 1
 sleep 10
-curl --fail http://localhost:8080/echo/hello
-curl --fail http://localhost:8500/v1/agent/services
-curl --fail http://localhost:8500/v1/health/checks/echo
+curl --fail http://localhost:8080/echo/hello || exit 1
+curl --fail http://localhost:8500/v1/agent/services || exit 1
+curl --fail http://localhost:8500/v1/health/checks/echo || exit 1
 docker-compose -f api-tests/docker-compose.yml down
