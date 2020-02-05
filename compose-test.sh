a1b7a73b43ec12e30aebad3eed64e47141288d19
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2020-02-05 12:49:49 -0300

    Take in consideration code review

--- compose-test.sh
+++ compose-test.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+set -e
+
 if [[ -z "${TRAVIS_PULL_REQUEST_BRANCH}" ]]; then
   echo "BRANCH_NAME=${TRAVIS_BRANCH}" > .env
 else
   echo "BRANCH_NAME=${TRAVIS_PULL_REQUEST_BRANCH}" > .env
 fi
-docker-compose -f api-tests/docker-compose.yml up -d || exit 1
+docker-compose -f api-tests/docker-compose.yml up -d
 sleep 10
-curl --fail http://localhost:8080/echo/hello || exit 1
-curl --fail http://localhost:8500/v1/agent/services || exit 1
-curl --fail http://localhost:8500/v1/health/checks/echo || exit 1
+curl --fail http://localhost:8080/echo/hello
+curl --fail http://localhost:8500/v1/agent/services
+curl --fail http://localhost:8500/v1/health/checks/echo
 docker-compose -f api-tests/docker-compose.yml down
