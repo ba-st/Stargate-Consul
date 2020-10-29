f197d0da9e330214e9bb57f2d872f142eadb016e
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2020-10-29 14:32:27 -0300

    Update dockerfile, use Pharo 8 and the Stargate facilities

--- api-tests/health-check.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-curl --fail --request POST \
-  --header "Authorization: Bearer $HEALTH_CHECK_TOKEN" \
-  --header "Accept: application/vnd.stargate.health-check.summary+json;version=1.0.0" \
-  http://localhost:"${PORT:-8080}"/operations/health-check || exit 1
