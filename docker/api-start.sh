b1503ba66816c7b4bfb759488208e0a8dcc74cdd
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2020-10-29 14:58:34 -0300

    Fix shellcheck warnings

--- docker/api-start.sh
+++ docker/api-start.sh
@@ -3,7 +3,7 @@
 exec /opt/pharo/pharo \
 	/opt/Stargate-Consul-Example-API/Pharo.image \
 	stargate-consul-example \
-    --public-URL=${PUBLIC_URL} \
+    --public-URL="${PUBLIC_URL}" \
 		"${PORT:+--port=$PORT}" \
 		"${OPERATIONS_SECRET:+--operations-secret=$OPERATIONS_SECRET}" \
-    --consul-agent-location=${CONSUL_AGENT_LOCATION}
+    --consul-agent-location="${CONSUL_AGENT_LOCATION}"
