b107bdf20231288edeed6902014e862cbf87e871
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2022-02-18 08:25:10 -0300

    Improve compose test script

--- compose-test.sh
+++ compose-test.sh
@@ -7,14 +7,22 @@ if [[ -z "${GITHUB_HEAD_REF##*/}" ]]; then
 else
   echo "BRANCH_NAME=${GITHUB_HEAD_REF##*/}" > .env
 fi
-docker-compose -f api-tests/docker-compose.yml up -d
+echo "Building API"
+docker-compose -f api-tests/docker-compose.yml build api
+echo "Starting Consul Agent"
+docker-compose -f api-tests/docker-compose.yml up -d consul-agent
+sleep 1
+echo "Starting API"
+docker-compose -f api-tests/docker-compose.yml up -d api
 sleep 10
+echo "Testing API"
 curl --fail http://localhost:8080/echo/hello
 curl --fail http://localhost:8500/v1/agent/services
 curl --fail http://localhost:8500/v1/health/checks/echo
 HEALTH_STATUS=$(curl -s http://localhost:8500/v1/health/checks/echo | jq '.[0].Status')
-if [ "$HEALTH_STATUS" != "passing" ]; then
-  echo "ERROR: Echo service is unhealthy"
+echo "$HEALTH_STATUS"
+if [ "$HEALTH_STATUS" != '"passing"' ]; then
+  echo "Error: Echo service is unhealthy" >&2
   docker-compose -f api-tests/docker-compose.yml down
   exit 1
 fi
