f4cbb6542a90a8aee250036259e1c8079cfd4aad
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2024-04-15 15:32:16 -0300

    Add support for GS64 3.7.1

--- compose-test-gs64.sh
+++ compose-test-gs64.sh
@@ -2,17 +2,18 @@
 
 set -e
 
+readonly GS64_COMPOSE_FILE="api-tests/gs64/docker-compose.yml"
 echo "Building API"
-docker compose -f api-tests/gs64/docker-compose.yml build api
+docker compose -f "$GS64_COMPOSE_FILE" build api
 echo "Starting Stone"
-docker compose -f api-tests/gs64/docker-compose.yml up -d stone
+docker compose -f "$GS64_COMPOSE_FILE" up -d stone
 echo "Starting Consul Agent"
-docker compose -f api-tests/gs64/docker-compose.yml up -d consul-agent
+docker compose -f "$GS64_COMPOSE_FILE" up -d consul-agent
 sleep 2
 echo "Installing support code"
 docker exec -i -u gemstone gs64-stone-1 ./load-rowan-project.sh Stargate-Consul Stargate-Consul-Examples
 echo "Starting API"
-docker compose -f api-tests/gs64/docker-compose.yml up -d api
+docker compose -f "$GS64_COMPOSE_FILE" up -d api
 sleep 10
 echo "Testing API"
 curl --fail http://localhost:8080/echo/hello
@@ -22,7 +23,7 @@ HEALTH_STATUS=$(curl -s http://localhost:8500/v1/health/checks/echo | jq '.[0].S
 echo "$HEALTH_STATUS"
 if [ "$HEALTH_STATUS" != '"passing"' ]; then
   echo "Error: Echo service is unhealthy" >&2
-  docker compose -f api-tests/gs64/docker-compose.yml down
+  docker compose -f "$GS64_COMPOSE_FILE" down
   exit 1
 fi
-docker compose -f api-tests/gs64/docker-compose.yml down
+docker compose -f "$GS64_COMPOSE_FILE" down
